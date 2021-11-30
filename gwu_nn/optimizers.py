2af4911e4e4f94ca72e3161ac48df403d2559261
Charles Peeke <peeke@Charless-MacBook-Pro.local>
2021-11-30 18:47:10 -0500

    [SGD] Add Decay Functionality

--- gwu_nn/optimizers.py
+++ gwu_nn/optimizers.py
@@ -22,12 +22,24 @@ class Optimizer(ABC):
 class SGD(Optimizer):
 
     # Initialize optimizer - 
-    def __init__(self, learning_rate=1.0):
+    def __init__(self, learning_rate=1.0, decay=0.0):
         self.learning_rate = learning_rate
+        self.updated_learning_rate = learning_rate
+        self.decay = decay
+        self.iterations = 0
+    
+    # Called once before the optimize function
+    def before_optimize(self):
+        if self.decay:
+            self.updated_learning_rate = self.learning_rate * (1.0 / (1.0 + (self.decay * self.iterations)))
 
     # Optimize (Update Layer Weights/Bias)
     def optimize(self, layer):
-        layer.weights += -self.learning_rate * self.weights_error
+        layer.weights += -self.updated_learning_rate * self.weights_error
         if layer.add_bias:
-            layer.bias -= self.learning_rate * self.output_error
+            layer.bias -= self.updated_learning_rate * self.output_error
+
+    # Called once after optimize function
+    def after_optimize(self):
+        self.iterations += 1
 

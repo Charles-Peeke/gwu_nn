8373456d68907b0a38b55370e27d9525393bd7b3
Charles Peeke <peeke@Charless-MacBook-Pro.local>
2021-11-30 21:43:29 -0500

    [SGD] Add Momentum functionality to SGD Optimizer

--- gwu_nn/optimizers.py
+++ gwu_nn/optimizers.py
@@ -22,11 +22,12 @@ class Optimizer(ABC):
 class SGD(Optimizer):
 
     # Initialize optimizer - 
-    def __init__(self, learning_rate=1.0, decay=0.0):
+    def __init__(self, learning_rate=1.0, decay=0.0, momentum=0.0):
         self.learning_rate = learning_rate
         self.updated_learning_rate = learning_rate
         self.decay = decay
         self.iterations = 0
+        self.momentum = momentum
     
     # Called once before the optimize function
     def before_optimize(self):
@@ -35,9 +36,26 @@ class SGD(Optimizer):
 
     # Optimize (Update Layer Weights/Bias)
     def optimize(self, layer):
-        layer.weights += -self.updated_learning_rate * self.weights_error
-        if layer.add_bias:
-            layer.bias -= self.updated_learning_rate * self.output_error
+
+        if self.momentum:
+
+            # Create momentums if not already present
+            if not hasattr(layer, 'weight_momentums'):
+                layer.weight_momentums = np.zeros_like(layer.weights)
+                layer.bias_momentums = np.zeros_like(layer.biases)
+            
+            weight_updates = self.momentum * layer.weight_momentums - self.current_learning_rate * layer.weights_error
+            layer.weight_momentums = weight_updates
+
+            # Build bias updates
+            bias_updates = self.momentum * layer.bias_momentums - self.current_learning_rate * layer.output_error
+            layer.bias_momentums = bias_updates
+
+        else:
+                
+            layer.weights += -self.updated_learning_rate * layer.weights_error
+            if layer.add_bias:
+                layer.bias -= self.updated_learning_rate * layer.output_error
 
     # Called once after optimize function
     def after_optimize(self):

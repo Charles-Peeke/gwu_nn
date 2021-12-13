6c61dab6562846b758f00bc5a8d43aee625950e3
Charles Peeke <peeke@Charless-MacBook-Pro.local>
2021-12-13 16:01:04 -0500

    Refactoring

--- gwu_nn/optimizers.py
+++ gwu_nn/optimizers.py
@@ -51,8 +51,9 @@ class SGD(Optimizer):
             layer.weight_momentums = weight_updates
 
             # Build bias updates
-            bias_updates = self.momentum * layer.bias_momentums - self.updated_learning_rate * layer.output_error
-            layer.bias_momentums = bias_updates
+            if layer.add_bias:
+                bias_updates = self.momentum * layer.bias_momentums - self.updated_learning_rate * layer.output_error
+                layer.bias_momentums = bias_updates
 
         else:
                 
@@ -95,11 +96,14 @@ class Adagrad(Optimizer):
 
         # Update the layer caches
         layer.weight_cache += layer.weights_error**2
-        layer.bias_cache += layer.output_error**2
-
+        
         # Parameter updates (Modified SGD)
         layer.weights -= self.updated_learning_rate * layer.weights_error / (np.sqrt(layer.weight_cache) + self.epsilon)
-        layer.bias -= self.updated_learning_rate * layer.output_error / (np.sqrt(layer.bias_cache) + self.epsilon)
+        
+        # Same process for bias
+        if layer.add_bias:
+            layer.bias_cache += layer.output_error**2
+            layer.bias -= self.updated_learning_rate * layer.output_error / (np.sqrt(layer.bias_cache) + self.epsilon)
 
     # Called once after optimize function
     @classmethod
@@ -137,11 +141,13 @@ class RMSprop(Optimizer):
 
         # Update the layer caches
         layer.weight_cache = self.beta * layer.weight_cache + (1 - self.beta) * layer.weights_error**2
-        layer.bias_cache = self.beta * layer.bias_cache + (1 - self.beta) * layer.output_error**2
-
         # Parameter updates (Modified SGD)
         layer.weights -= self.updated_learning_rate * layer.weights_error / (np.sqrt(layer.weight_cache) + self.epsilon)
-        layer.bias -= self.updated_learning_rate * layer.output_error / (np.sqrt(layer.bias_cache) + self.epsilon)
+
+        # Same process for bias
+        if layer.add_bias:
+            layer.bias_cache = self.beta * layer.bias_cache + (1 - self.beta) * layer.output_error**2
+            layer.bias -= self.updated_learning_rate * layer.output_error / (np.sqrt(layer.bias_cache) + self.epsilon)
 
     # Called once after optimize function
     @classmethod
@@ -181,23 +187,26 @@ class Adam(Optimizer):
 
         # Update momentums
         layer.weight_momentums = self.beta_momentum * layer.weight_momentums + (1 - self.beta_momentum) * layer.weights_error
-        layer.bias_momentums = self.beta_momentum * layer.bias_momentums + (1 - self.beta_momentum) * layer.output_error
-        
+
         # Calculate Updated Momentums
         weight_momentums_calculated = layer.weight_momentums / (1 - self.beta_momentum ** (self.iterations + 1))
-        bias_momentums_calculated = layer.bias_momentums / (1 - self.beta_momentum ** (self.iterations + 1))
-        
+
         # Update caches
         layer.weight_cache = self.beta_cache * layer.weight_cache + (1 - self.beta_cache) * layer.weights_error**2
-        layer.bias_cache = self.beta_cache * layer.bias_cache + (1 - self.beta_cache) * layer.output_error**2
-        
+
         # Calculate Updated Caches
         weight_cache_calculated = layer.weight_cache / (1 - self.beta_cache ** (self.iterations + 1))
-        bias_cache_calculated = layer.bias_cache / (1 - self.beta_cache ** (self.iterations + 1))
 
         # Parameter updates (Modified SGD)
         layer.weights -= self.current_learning_rate * weight_momentums_calculated / (np.sqrt(weight_cache_calculated) + self.epsilon)
-        layer.bias -= self.current_learning_rate * bias_momentums_calculated / (np.sqrt(bias_cache_calculated) + self.epsilon)
+
+        # Same process for bias
+        if layer.add_bias:
+            layer.bias_momentums = self.beta_momentum * layer.bias_momentums + (1 - self.beta_momentum) * layer.output_error
+            bias_momentums_calculated = layer.bias_momentums / (1 - self.beta_momentum ** (self.iterations + 1))
+            layer.bias_cache = self.beta_cache * layer.bias_cache + (1 - self.beta_cache) * layer.output_error**2
+            bias_cache_calculated = layer.bias_cache / (1 - self.beta_cache ** (self.iterations + 1))
+            layer.bias -= self.current_learning_rate * bias_momentums_calculated / (np.sqrt(bias_cache_calculated) + self.epsilon)
 
     # Called once after optimize function
     @classmethod
